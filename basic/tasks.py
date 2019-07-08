d44f3e251e5f024adfd15bb8c7d521d7864b271b
Hank Ehly <henry.ehly@gmail.com>
2019-07-08 09:55:22 +0900

    Add basic app example

---
+++ basic/tasks.py
@@ -0,0 +1,27 @@
+from celery import Celery
+
+# first argument is the name of the current module
+# broker is used to send and receive messages
+# backend is used to store results and the status of results.
+# Without it, you cannot retrieve the return value of the task
+app = Celery('tasks', broker='redis://', backend='redis://')
+
+
+@app.task
+def add(x, y):
+    return x + y
+
+# >>> from tasks import add
+# >>> add.delay(4, 4)
+# <AsyncResult: 9e0e1c5e-030f-46df-9040-4c91ae1ebf4b>
+#
+# Received task: basic.tasks.add[9e0e1c5e-030f-46df-9040-4c91ae1ebf4b]
+# Task basic.tasks.add[9e0e1c5e-030f-46df-9040-4c91ae1ebf4b] succeeded in 0.0003735700000007114s: 8
+#
+#
+# >>> result = add.delay(5, 5)
+# >>> result.ready()
+# True
+# >>> result.get()
+# 10
+#
